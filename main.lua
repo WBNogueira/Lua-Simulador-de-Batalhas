3fccedfb627fc92cfd34be1a7c97c311f5908830
Wagner <wbnogueira3@gmail.com>
2024-05-31 17:14:36 -0300

    Definindo jogador e mosntro

--- main.lua
+++ main.lua
@@ -1,3 +1,7 @@
+-- Dependencies
+local player = require("definitions.player")
+local colossus = require("definitions.colossus")
+
 -- Habilitar UTF-8 no terminal
 os.execute("chcp 65001")
 
@@ -25,8 +29,11 @@ print([[
 ]])
 
 -- Obter definição do jogador
+player.health = player.health - 2
+print(string.format("A vida do jogador é %d/%d", player.health, player.maxHealth))
 
 -- Obter definição do monstro
+local boss = colossus
 
 -- Apresentar o monstro
 
