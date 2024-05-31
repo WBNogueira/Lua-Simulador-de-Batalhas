6a44e7068df4634d0b8e713d1f9c051cc215d9b7
Wagner <wbnogueira3@gmail.com>
2024-05-31 16:41:35 -0300

    Criação do arquivo com um Header

---
+++ main.lua
@@ -0,0 +1,33 @@
+-- Habilitar UTF-8 no terminal
+os.execute("chcp 65001")
+
+-- Header
+
+
+print([[
+=====================================================================
+                _
+     _         | |
+    | | _______| |---------------------------------------------\
+    |:-)_______|==[]============================================>
+    |_|        | |---------------------------------------------/
+               |_|
+
+                -----------------------------------------
+
+                        ⚔ SIMULADOR DE BATALHA ⚔
+
+=====================================================================
+
+            Você empunha sua espada e se prepara para lutar.
+                            É hora da batalha!
+
+]])
+
+-- Obter definição do jogador
+
+-- Obter definição do monstro
+
+-- Apresentar o monstro
+
+-- Começar o loop da batalha
